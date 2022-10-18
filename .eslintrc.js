12ea63c071e739e8ff4b9229d7276d2006504921
mydatahack <thon6thon6@hotmail.com>
2022-10-18 16:39:49 +1100

    fix(rules): remove styled component name rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -45,7 +45,6 @@ const eslint = {
     'eslint-plugin-mdh-react/ban-parse-float': 2,
     'eslint-plugin-mdh-react/force-typed-api-requests': 2,
     'eslint-plugin-mdh-react/ban-raw-apollo-use-subscription': 2,
-    'eslint-plugin-mdh-react/styled-components-naming': 2,
     'react/react-in-jsx-scope': 0,
     // note we must disable the base rule as it can report incorrect errors
     'no-use-before-define': 'off',
